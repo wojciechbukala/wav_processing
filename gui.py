fa1c45d7841363808e7129468aacd8b923e97f3d
wojciech <263479@student.pwr.edu.pl>
2024-03-15 22:07:29 +0100

    pages

--- gui.py
+++ gui.py
@@ -50,12 +50,97 @@ class GUI:
     def drop_wav_window_reset(self):
         self.label1.destroy()
 
+    def parameters_page(self, cwav):
+        #create StringVar() for labels
+        header = tk.StringVar(self.wav_parameters, cwav.header_to_string())
+        header_chunk = tk.StringVar(self.wav_parameters, cwav.header_chunk_to_string())
+        data_chunk = tk.StringVar(self.wav_parameters, cwav.data_to_string())
+        format_chunk = tk.StringVar(self.wav_parameters, cwav.format_to_string())
+        meta_chunk = tk.StringVar(self.wav_parameters, cwav.meta_to_string())
+
+        #Header Label
+        self.header_lbl = tk.Label(self.wav_parameters, textvariable=header)
+        self.header_lbl.grid(row=0, column=0, columnspan=2)
+
+        #Header Chunk Label
+        self.header_chunk_frame = tk.Frame(self.wav_parameters)
+        self.header_chunk_frame.grid(row=1, column=0)
+        header_chunk_title = tk.Label(self.header_chunk_frame, text="HEADER CHUNK")
+        header_chunk_title.pack()
+        header_chunk_lbl = tk.Label(self.header_chunk_frame, textvariable=header_chunk)
+        header_chunk_lbl.pack()
+
+        #Format Chunk Label
+        self.format_chunk_frame = tk.Frame(self.wav_parameters)
+        self.format_chunk_frame.grid(row=1, column=1)
+        format_chunk_title = tk.Label(self.format_chunk_frame, text="FORMAT CHUNK")
+        format_chunk_title.pack()
+        format_chunk_lbl = tk.Label(self.format_chunk_frame, textvariable=format_chunk)
+        format_chunk_lbl.pack()
+
+        #Data Chunk Label
+        self.data_chunk_frame = tk.Frame(self.wav_parameters)
+        self.data_chunk_frame.grid(row=2, column=0)
+        data_chunk_title = tk.Label(self.data_chunk_frame, text="DATA CHUNK")
+        data_chunk_title.pack()
+        data_chunk_lbl = tk.Label(self.data_chunk_frame, textvariable=data_chunk)
+        data_chunk_lbl.pack()
+
+        #Metadata Chunk Label
+        self.meta_chunk_frame = tk.Frame(self.wav_parameters)
+        self.meta_chunk_frame.grid(row=2, column=1)
+        meta_chunk_title = tk.Label(self.meta_chunk_frame, text="DATA CHUNK")
+        meta_chunk_title.pack()
+        meta_chunk_lbl = tk.Label(self.meta_chunk_frame, textvariable=meta_chunk)
+        meta_chunk_lbl.pack()
+
+    def destory_pages(self):
+        for child in self.wav_parameters.winfo_children():
+            child.destroy()
+
+    def plots_page(self):
+        pass
+
+    def next_page(self):
+        self.current_page += 1
+        if self.current_page == 3:
+            self.current_page = 0
+
+        self.destory_pages()
+
+        match self.current_page:
+            case 0:
+                self.parameters_page(self.cwav)
+            case 1:
+                pass
+            case 2:
+                pass
+            
+
+    def prev_page(self):
+        self.current_page -= 1
+        if self.current_page == -1:
+            self.current_page = 2
+
+        self.destory_pages()
+
+        match self.current_page:
+            case 0:
+                self.parameters_page(self.cwav)
+            case 1:
+                pass
+            case 2:
+                pass
+
     def wav_file_window(self):
-        self.wav_player = tk.Frame(self.root, bg="blue")
-        self.wav_player.pack(side = BOTTOM, fill="x")
+        self.navigation_and_title = tk.Frame(self.root, bg="blue")
+        self.navigation_and_title.pack(side = TOP, fill="x")
 
         self.wav_parameters = tk.Frame(self.root, bg="green")
-        self.wav_parameters.pack(side= TOP, fill="x")
+        self.wav_parameters.pack(fill="both", expand=True)
+
+        self.wav_player = tk.Frame(self.root, bg="blue")
+        self.wav_player.pack(side = BOTTOM, fill="x")
 
         #images for play and pause music buttons
         self.play_btn_image = tk.PhotoImage(file='images/play.png')
@@ -78,34 +163,32 @@ class GUI:
         pygame.mixer.init()
 
         #create check_wav object
-        cwav = check_wav.Check_wav(self.wav_file)
+        self.cwav = check_wav.Check_wav(self.wav_file)
+        self.parameters_page(self.cwav)
+        self.current_page = 0
 
-        #create StringVar() for labels
-        header = tk.StringVar(self.wav_parameters, cwav.header_to_string())
-        header_chunk = tk.StringVar(self.wav_parameters, cwav.header_chunk_to_string())
-        data_chunk = tk.StringVar(self.wav_parameters, cwav.data_to_string())
-        format_chunk = tk.StringVar(self.wav_parameters, cwav.format_to_string())
-        meta_chunk = tk.StringVar(self.wav_parameters, cwav.meta_to_string())
+        #images for play and pause music buttons
+        self.next_page_btn_image = tk.PhotoImage(file='images/next_page.png')
+        self.prev_page_btn_image = tk.PhotoImage(file='images/prev_page.png')
 
-        #Header Label
-        self.header_lbl = tk.Label(self.wav_parameters, textvariable=header)
-        self.header_lbl.pack()
+        # Previous page button
+        self.prev_page_btn = tk.Button(self.navigation_and_title, image=self.prev_page_btn_image,
+                                        command=self.prev_page)
+        self.prev_page_btn.pack(side=LEFT)
 
-        #Header Chunk Label
-        self.header_chunk_lbl = tk.Label(self.wav_parameters, textvariable=header_chunk)
-        self.header_chunk_lbl.pack()
+        # Title
+        self.title_label = tk.Label(self.navigation_and_title, text=".wav Processing App", font=('Arial', 24))
+        self.title_label.pack(side=LEFT, padx= 170)
 
-        #Format Chunk Label
-        self.format_chunk_lbl = tk.Label(self.wav_parameters, textvariable=format_chunk)
-        self.format_chunk_lbl.pack()
+        # Next page button
+        self.next_page_btn = tk.Button(self.navigation_and_title, image=self.next_page_btn_image,
+                                        command=self.next_page)
+        self.next_page_btn.pack(side=RIGHT)
 
-        #Data Chunk Label
-        self.data_chunk_lbl = tk.Label(self.wav_parameters, textvariable=data_chunk)
-        self.data_chunk_lbl.pack()
+        # Fit size of buttons
+        self.next_page_btn.config(width=64, height=64)
+        self.prev_page_btn.config(width=64, height=64)
 
-        #Metadata Chunk Label
-        self.meta_chunk_lbl = tk.Label(self.wav_parameters, textvariable=meta_chunk)
-        self.meta_chunk_lbl.pack()
 
         # Register a callback to stop audio when the window is closed
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
